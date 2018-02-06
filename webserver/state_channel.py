78283e9ccd81c72f4b013b23a3e4148d8939aaad
jstoxrocky <joestox@gmail.com>
2018-02-06 17:21:45 -0500

    Better hex

--- webserver/state_channel.py
+++ webserver/state_channel.py
@@ -5,6 +5,10 @@ from web3 import (
 from toolz.dicttoolz import (
     valmap,
 )
+from eth_utils import (
+    encode_hex,
+    int_to_big_endian,
+)
 
 
 def sign(private_key, contract, user, *values):
@@ -12,6 +16,6 @@ def sign(private_key, contract, user, *values):
     msg = Web3.soliditySha3(abi_types, [contract, user] + list(values))
     signed = Account.sign(msg, private_key)
     signed = valmap(lambda x: x.hex() if isinstance(x, bytes) else x, signed)
-    signed['r'] = Web3.toHex(signed['r'])
-    signed['s'] = Web3.toHex(signed['s'])
+    signed['r'] = encode_hex(int_to_big_endian(signed['r']))
+    signed['s'] = encode_hex(int_to_big_endian(signed['s']))
     return signed
