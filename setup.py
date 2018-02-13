0b2443ab2c51dd922ca5e363e08f783ae5cdf095
jstoxrocky <joestox@gmail.com>
2018-02-12 20:36:02 -0500

    Change version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='endpoints',
-    version="0.1",
+    version="1.0.0",
     description="endpoints: Server-side endpoints for 0x2048",
     include_package_data=True,
     packages=find_packages(),
