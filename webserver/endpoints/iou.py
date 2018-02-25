1f3cf09638b773791c88684aa9ea8ed814775918
jstoxrocky <joestox@gmail.com>
2018-02-25 15:02:44 -0500

    Add GET method to IOU endpoint

--- webserver/endpoints/iou.py
+++ webserver/endpoints/iou.py
@@ -35,25 +35,31 @@ class mock_db_connection:
         return cls.value
 
 
-@blueprint.route('/iou', methods=['POST'])
-@cross_origin(origins=ORIGINS, methods=['POST'], supports_credentials=True)
+@blueprint.route('/iou', methods=['GET', 'POST'])
+@cross_origin(origins=ORIGINS, methods=['GET', 'POST'],
+              supports_credentials=True)
 def iou():
-    # Ensure user has not already paid
-    if session.get('has_paid', False):
-        raise UnexpectedPayment
-    # Validate payload
-    payload = request.get_json()
-    if IOUSchema().validate(payload):
-        raise ValidationError
-    # Validate IOU
-    success = state_channel.validate_iou(payload)
-    if not success:
-        raise UnexpectedSignature
-    # Ensure the preimage value is strictly greater than the db value
-    db_value = mock_db_connection.execute("""SELECT * FROM tbl;""")
-    if payload['value'] <= db_value:
-        raise IOUPaymentTooLow
-    #  TODO:
-    #  (1) Store IOU in db
-    session['has_paid'] = True
-    return jsonify({'success': True})
+    if request.method == 'GET':
+        # Ensure the preimage value is strictly greater than the db value
+        db_value = mock_db_connection.execute("""SELECT * FROM tbl;""")
+        return jsonify({'value': db_value})
+    elif request.method == 'POST':
+        # Ensure user has not already paid
+        if session.get('has_paid', False):
+            raise UnexpectedPayment
+        # Validate payload
+        payload = request.get_json()
+        if IOUSchema().validate(payload):
+            raise ValidationError
+        # Validate IOU
+        success = state_channel.validate_iou(payload)
+        if not success:
+            raise UnexpectedSignature
+        # Ensure the preimage value is strictly greater than the db value
+        db_value = mock_db_connection.execute("""SELECT * FROM tbl;""")
+        if payload['value'] <= db_value:
+            raise IOUPaymentTooLow
+        #  TODO:
+        #  (1) Store IOU in db
+        session['has_paid'] = True
+        return jsonify({'success': True})
