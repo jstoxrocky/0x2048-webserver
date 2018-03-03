cc0c5391e94b17ed36d9700a9b200cb28a26fce7
jstoxrocky <joestox@gmail.com>
2018-03-03 16:34:59 -0500

    Handle IOUs signed with eth_signTypedData

--- tests/unit/test_state_channel.py
+++ tests/unit/test_state_channel.py
@@ -1,10 +1,13 @@
 import pytest
 from webserver.state_channel import (
     sign,
-    recover_message,
+    recover,
     int_to_hex,
     solidity_keccak,
     validate_iou,
+    raw_sign,
+    hash_typed_data,
+    sign_typed_data,
 )
 from webserver.config import (
     PRIV,
@@ -23,16 +26,22 @@ def test_sign(user):
     assert isinstance(output['s'], str)
 
 
+def test_raw_sign(user):
+    msg = b'Joey to the world!'
+    signature = raw_sign(msg, user.privateKey)
+    assert signature.to_hex() == '0xc940c68a311665c9329c22e3b858bcb91f9c7dc95844c61401ad61f28ba5274e7b1b5d673f6e9494df2fa1d586d49add102f736ac02cc28e6aff221e0fc09a5901'  # noqa: E501
+
+
 def test_int_to_hex():
     assert len(int_to_hex(16**0)) % 2 == 0
     assert len(int_to_hex(16**1)) % 2 == 0
     assert len(int_to_hex(16**2)) % 2 == 0
 
 
-def test_recover_message(user):
+def test_recover(user):
     msg = solidity_keccak(ARCADE_ADDR, user.address, 1, 2, 3)
     signed = Account.sign(msg, user.privateKey)
-    signer = recover_message(msg, signed['signature'])
+    signer = recover(signed['messageHash'], signed['signature'])
     assert signer == user.address
 
 
@@ -45,12 +54,16 @@ def test_solidity_keccak(user):
 
 def test_validate_iou(user):
     value = 1
-    msg = solidity_keccak(ACCOUNT_ADDR, user.address, value)
-    signed = sign(msg, user.privateKey)
+    msg_params = [
+        {'type': 'address', 'name': 'destination', 'value': ACCOUNT_ADDR},
+        {'type': 'address', 'name': 'user', 'value': user.address},
+        {'type': 'uint256', 'name': 'value', 'value': value},
+    ]
+    signature = sign_typed_data(msg_params, user.privateKey)
     iou = {
         'user': user.address,
         'value': value,
-        'signature': signed['signature'],
+        'signature': signature,
     }
     success = validate_iou(iou)
     assert success
@@ -85,3 +98,13 @@ def test_validate_iou_bad_signer(user, user2):
     }
     success = validate_iou(iou)
     assert not success
+
+
+def test_sign_typed_data(user):
+    msg_params = [
+        {'type': 'uint256', 'name': 'x', 'value': 1},
+    ]
+    msg_hash = hash_typed_data(msg_params)
+    signature = sign_typed_data(msg_params, user.privateKey)
+    signer = recover(msg_hash, signature)
+    assert signer == user.address
