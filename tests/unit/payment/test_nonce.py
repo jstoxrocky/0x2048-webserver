522232c383e72a5de8142f143cb7901390732558
jstoxrocky <joestox@gmail.com>
2018-03-26 18:20:56 -0400

    Reset payment value on new request for nonce

--- tests/unit/payment/test_nonce.py
+++ tests/unit/payment/test_nonce.py
@@ -1,11 +1,15 @@
 import json
-from webserver import exceptions
 
 
 def test_nonce_success(mocker, app, api_prefix, user):
     """
-    It should succeed
+    It should succeed and overwrite session.nonce and session.paid
     """
+    with app as c:
+        with c.session_transaction() as sess:
+            sess['nonce'] = 987654
+            sess['paid'] = True
+
     nonce = 123456
     generate_random_nonce = mocker.patch('webserver.endpoints.payment.state_channel.generate_random_nonce')  # noqa: E501
     generate_random_nonce.return_value = nonce
@@ -18,32 +22,4 @@ def test_nonce_success(mocker, app, api_prefix, user):
     with app as c:
         with c.session_transaction() as sess:
             assert sess['nonce'] == nonce
-
-
-def test_nonce_already_paid(app, api_prefix, user):
-    """
-    It should fail with exception UnexpectedPaymentAttempt
-    """
-    with app as c:
-        with c.session_transaction() as sess:
-            sess['paid'] = True
-    response = app.get(
-        api_prefix + '/nonce',
-    )
-    output = json.loads(response.data)
-    assert output['message'] == exceptions.UnexpectedPaymentAttempt.message
-
-
-def test_nonce_already_generated(app, api_prefix, user):
-    """
-    It should fail with exception UnexpectedNonceGenerationAttempt
-    """
-    with app as c:
-        with c.session_transaction() as sess:
-            sess['paid'] = False
-            sess['nonce'] = 987654
-    response = app.get(
-        api_prefix + '/nonce',
-    )
-    output = json.loads(response.data)
-    assert output['message'] == exceptions.UnexpectedNonceGenerationAttempt.message  # noqa: E501
+            assert not sess['paid']
