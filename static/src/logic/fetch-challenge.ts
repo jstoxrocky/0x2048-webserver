acfc7f71d8018e264715a94d3e4f4953e65a1bde
Joey Stockermans <joestox@gmail.com>
2020-04-04 11:41:42 -0400

    Add frontend (#86)

--- static/src/logic/fetch-challenge.ts
+++ static/src/logic/fetch-challenge.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { Protected, ChallengeData } from '../types';
 import protectedCall from './protected-call';
 
-const baseURL = 'http://0.0.0.0:5000/';
+const baseURL = '//0.0.0.0:5000/';
 const api = axios.create({ baseURL });
 
 type ApiResponse = AxiosResponse<ChallengeData>;
